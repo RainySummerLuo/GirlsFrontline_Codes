4ebcc9584702d280159eb80d903523b59068359d
Zichen Luo <iluo@163.com>
2019-03-12 16:34:41 +0800

    Add features
    1. showing progress
    2. automatically create directory if not exist

--- Program.cs
+++ Program.cs
@@ -15,15 +15,27 @@ namespace GirlsFrontline_Downloader
         {
             var dirPath = Environment.CurrentDirectory;
             dirPath = Path.Combine(Directory.GetParent(Directory.GetParent(dirPath).ToString()) + @"\data\");
+            if (!Directory.Exists(Path.Combine(dirPath + @"download\")))
+            {
+                Directory.CreateDirectory(Path.Combine(dirPath + @"download\"));
+            }
             Console.WriteLine("Please choose which server to download (en/cn):");
             string serverInput;
             do
             {
                 serverInput = Console.ReadLine();
             } while (serverInput != "en" && serverInput != "cn");
+            if (!Directory.Exists(Path.Combine(dirPath + @"download\" + serverInput + @"\")))
+            {
+                Directory.CreateDirectory(Path.Combine(dirPath + @"download\" + serverInput + @"\"));
+            }
+            Console.WriteLine();
+
             var filePath = Path.Combine(dirPath + @"\wiki-" + serverInput + ".html");
-            /*try
-            {*/
+            /*
+            try
+            {
+            */
             var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
             var streamReader = new StreamReader(fileStream, Encoding.Default);
             fileStream.Seek(0, SeekOrigin.Begin);
@@ -32,8 +44,19 @@ namespace GirlsFrontline_Downloader
             if (serverInput == "cn") serverName = "http://www.gfwiki.org";
             if (serverInput == "en") serverName = "https://www.gfwiki.com";
 
+            var lineCount = 0;
+            var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            var sr = new StreamReader(fs, Encoding.Default);
+            while (sr.ReadLine() != null)
+            {
+                lineCount++;
+            }
+            fs.Close();
+            sr.Close();
             var content = streamReader.ReadLine();
-            var count = 0;
+            var downloadCount = 0;
+            var totalCount = 0;
+            Console.WriteLine("Start Download: totally " + lineCount + " items.");
             //var myWebClient = new WebClient();
             /*
             var handler = new WebRequestHandler();
@@ -45,6 +68,7 @@ namespace GirlsFrontline_Downloader
             client.DefaultRequestHeaders.Connection.Add("keep-alive");
             while (content != null)
             {
+                totalCount++;
                 var uriSplit = content.Split(Convert.ToChar("\""));
                 var url = serverName + uriSplit[1].Trim();
                 var localPath = url.Split('/')[6].Trim();
@@ -62,10 +86,11 @@ namespace GirlsFrontline_Downloader
                     if (strPicContent.Substring(0, 15) == "<!DOCTYPE html>") {
                         Console.WriteLine("[ERROR] " + url);
                     } else {
-                        count++;
-                        var fs = new FileStream(downloadPath, FileMode.Create);
-                        fs.Write(picContent, 0, picContent.Length);
-                        Console.WriteLine("[NO." + count + " Download] " + url);
+                        downloadCount++;
+                        var downloadFs = new FileStream(downloadPath, FileMode.Create);
+                        downloadFs.Write(picContent, 0, picContent.Length);
+                        var progress = (decimal)totalCount / lineCount * 100;
+                        Console.WriteLine("[" + progress.ToString("0.00") + "%][NO." + downloadCount + " Download] " + url);
                     }
                 }
                 content = streamReader.ReadLine();
