6fe91ff7778b8f7a21ed32e73fce34860734fe78
Zichen Luo <iluo@163.com>
2019-03-12 11:52:09 +0800

    Support Chinese file name and ignore download dir

--- Program.cs
+++ Program.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.IO;
-using System.Net;
+using System.Net.Http;
 using System.Text;
+using System.Web;
+using System.Threading.Tasks;
 
 // ReSharper disable once IdentifierTypo
 namespace GirlsFrontline_Downloader
 {
     internal static class Program
     {
-        private static void Main()
+        private static async Task Main()
         {
             var dirPath = Environment.CurrentDirectory;
             dirPath = Path.Combine(Directory.GetParent(Directory.GetParent(dirPath).ToString()) + @"\data\");
@@ -28,16 +30,20 @@ namespace GirlsFrontline_Downloader
                 fileStream.Seek(0, SeekOrigin.Begin);
 
                 var content = streamReader.ReadLine();
-                var myWebClient = new WebClient();
                 var count = 0;
+                //var myWebClient = new WebClient();
+                var client = new HttpClient();
                 while (content != null)
                 {
                     count++;
                     var uriSplit = content.Split(Convert.ToChar("\""));
                     var url = "http://www.gfwiki.org" + uriSplit[1].Trim();
                     var localPath = url.Split('/')[6].Trim();
-                    var downloadPath = Path.Combine(Path.Combine(dirPath + @"download\" + serverInput + @"\") + localPath);
-                    myWebClient.DownloadFile(url, downloadPath);
+                    var downloadPath = Path.Combine(Path.Combine(dirPath + @"download\" + serverInput + @"\") + HttpUtility.UrlDecode(localPath, Encoding.UTF8));
+                    //myWebClient.DownloadFile(url, downloadPath);
+                    var picContent = await client.GetByteArrayAsync(url);
+                    var fs = new FileStream(downloadPath, FileMode.Create);
+                    fs.Write(picContent, 0, picContent.Length);
                     Console.WriteLine("[NO."+ count +" Download] " + url);
                     content = streamReader.ReadLine();
                 }
