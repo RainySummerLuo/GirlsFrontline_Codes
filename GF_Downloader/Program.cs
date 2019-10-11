c6572b273ab8d9e693af4543b1dacdfc71d04cf3
Zichen Luo <iluo@163.com>
2019-10-11 22:45:22 +0800

    Fix the tool and add a JP wiki option.

--- GF_Downloader/Program.cs
+++ GF_Downloader/Program.cs
@@ -13,108 +13,122 @@ namespace GirlsFrontline_Downloader
     {
         private static async Task Main()
         {
-            var dirPath = Environment.CurrentDirectory;
-            dirPath = Path.Combine(dirPath + @"\data\");
-            if (!Directory.Exists(Path.Combine(dirPath + @"download\")))
+            var currentDirectory = Environment.CurrentDirectory;
+            var dataPath = Path.Combine(currentDirectory + @"\data\");
+            var downPath = Path.Combine(dataPath + @"\download\");
+            if (!Directory.Exists(Path.Combine(downPath)))
             {
-                Directory.CreateDirectory(Path.Combine(dirPath + @"download\"));
+                Directory.CreateDirectory(Path.Combine(downPath));
             }
-            Console.WriteLine("Please choose which server to download (en/cn):");
-            string serverInput;
+            Console.WriteLine("Girls' Frontline Characters Image Download Tool");
+            Console.WriteLine(@"by @RainySummer");
+            string server;
             do
             {
-                serverInput = Console.ReadLine();
-            } while (serverInput != "en" && serverInput != "cn");
-            if (!Directory.Exists(Path.Combine(dirPath + @"download\" + serverInput + @"\")))
+                Console.Write("\nPlease choose which server to download (cn/en/jp): ");
+                server = Console.ReadLine();
+            } while (server != "cn" && server != "en" && server != "jp");
+
+            var downServerPath = Path.Combine(downPath + @"\" + server + @"\");
+            if (!Directory.Exists(downServerPath))
             {
-                Directory.CreateDirectory(Path.Combine(dirPath + @"download\" + serverInput + @"\"));
+                Directory.CreateDirectory(downServerPath);
             }
             Console.WriteLine();
 
-            var filePath = Path.Combine(dirPath + @"\wiki-" + serverInput + ".html");
-            /*
-            try
+            var filePath = Path.Combine(dataPath + @"\wiki\" + server + ".html");
+            if (!File.Exists(filePath))
             {
-            */
-            var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
-            var streamReader = new StreamReader(fileStream, Encoding.Default);
-            fileStream.Seek(0, SeekOrigin.Begin);
-
+                Console.WriteLine("[ERROR] Download links file not found.");
+                Console.ReadLine();
+                return;
+            }
+            
             string serverName = null;
-            if (serverInput == "cn") serverName = "http://www.gfwiki.org";
-            if (serverInput == "en") serverName = "https://www.gfwiki.com";
-
+            // ReSharper disable once SwitchStatementMissingSomeCases
+            switch (server)
+            {
+                case "cn":
+                    serverName = "http://www.gfwiki.org";
+                    break;
+                case "en":
+                    serverName = "https://en.gfwiki.com";
+                    break;
+                case "jp":
+                    serverName = "https://wiki.gamerclub.jp";
+                    break;
+            }
+            
             var lineCount = 0;
-            var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
-            var sr = new StreamReader(fs, Encoding.Default);
-            while (sr.ReadLine() != null)
+            var countFs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            var countSr = new StreamReader(countFs, Encoding.Default);
+            while (countSr.ReadLine() != null)
             {
                 lineCount++;
             }
-            fs.Close();
-            sr.Close();
-            var content = streamReader.ReadLine();
-            var downloadCount = 0;
-            var totalCount = 0;
+            countFs.Close();
+            countSr.Close();
+            
+            var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            var sr = new StreamReader(fs, Encoding.UTF8);
+            fs.Seek(0, SeekOrigin.Begin);
+            string line;
+            var downCount = 0;
             Console.WriteLine("Start Download: totally " + lineCount + " items.");
-            //var myWebClient = new WebClient();
-            /*
-            var handler = new WebRequestHandler();
-            X509Certificate2 certificate = GetMyX509Certificate();
-            handler.ClientCertificates.Add(certificate);
-            */
+            
             var client = new HttpClient();
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
             client.DefaultRequestHeaders.Connection.Add("keep-alive");
-            while (content != null)
+            while ((line = sr.ReadLine()) != null)
             {
-                totalCount++;
-                var uriSplit = content.Split(Convert.ToChar("\""));
-                var url = serverName + uriSplit[1].Trim();
-                var localPath = url.Split('/')[6].Trim();
-                var downloadPath = Path.Combine(Path.Combine(dirPath + @"download\" + serverInput + @"\") +
-                                                HttpUtility.UrlDecode(localPath, Encoding.UTF8));
-                var picBool = true;
-                if (serverInput == "cn") {
-                    picBool = (localPath.Substring(0, 3) == "Pic");
-                }
-                if (picBool && File.Exists(downloadPath) == false)
+                downCount++;
+                if (!line.Contains("<a href=\"") || !line.Contains("images"))
                 {
-                    //myWebClient.DownloadFile(url, downloadPath);
-                    var picContent = await client.GetByteArrayAsync(url);
-                    var strPicContent = Encoding.Default.GetString(picContent);
-                    if (strPicContent.Substring(0, 15) == "<!DOCTYPE html>") {
-                        Console.WriteLine("[ERROR] " + url);
-                    } else {
-                        downloadCount++;
-                        var downloadFs = new FileStream(downloadPath, FileMode.Create);
-                        downloadFs.Write(picContent, 0, picContent.Length);
-                        var progress = (decimal)totalCount / lineCount * 100;
-                        Console.WriteLine("[" + progress.ToString("0.00") + "%][NO." + downloadCount + " Download] " + url);
-                    }
+                    continue;
                 }
-                content = streamReader.ReadLine();
+                var uriSplit = line.Split(new[]{"a href=\""}, StringSplitOptions.RemoveEmptyEntries);
+                uriSplit = uriSplit[1].Split('"');
+                var url = serverName + uriSplit[0].Trim();
+                var filenameSplit = url.Split('/');
+                var filename = filenameSplit[filenameSplit.Length - 1].Trim();
+                var downloadPath = Path.Combine(downServerPath +
+                                                HttpUtility.UrlDecode(filename, Encoding.UTF8));
+                var picBool = true;
+                if (server == "cn") {
+                    picBool = (filename.Substring(0, 3) == "Pic");
+                }
+                if (!picBool || File.Exists(downloadPath)) continue;
+                byte[] picContent;
+                try
+                {
+                    picContent = await client.GetByteArrayAsync(url);
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine("[ERROR] " + url + " | " + e.Message);
+                    File.Create(downloadPath);
+                    continue;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("[ERROR] " + url + " | " + e.Message);
+                    continue;
+                }
+                if (Encoding.Default.GetString(picContent).Substring(0, 15) == "<!DOCTYPE html>") {
+                    Console.WriteLine("[ERROR] " + url);
+                    File.Create(downloadPath);
+                    continue;
+                }
+                var downFs = new FileStream(downloadPath, FileMode.Create);
+                downFs.Write(picContent, 0, picContent.Length);
+                downFs.Close();
+                var progress = (decimal)downCount / lineCount * 100;
+                Console.WriteLine("[DOWNL] [" + progress.ToString("0.00") + "%] " + filename);
             }
-            fileStream.Close();
-            streamReader.Close();
-            Console.WriteLine("All the download links have been vistited.");
+            fs.Close();
+            sr.Close();
+            Console.WriteLine("All download links have been vistited.");
             Console.ReadLine();
-            /*}
-            catch
-            {
-                // ignored
-            }
-            finally
-            {
-                fileStream?.Close();
-                streamReader?.Close();
-            }*/
         }
-
-        /*
-        private static void Downloader(string url, string filepath)
-        {
-        }
-        */
     }
 }
